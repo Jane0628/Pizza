92c7a148c018b1ec6305616bc27448561e54a8ff
nyny567567 <ny567@naver.com>
2023-04-29 06:28:41 +0900

    230429 checkBoss 원상복구

--- pizza/src/com/pizza/check/checkBoss.java
+++ pizza/src/com/pizza/check/checkBoss.java
@@ -15,6 +15,14 @@ public class checkBoss implements AppService {
 		checkRealBoss();
 		int code = inputInteger();
 		
-		
+		if(code == 9999) {
+			service = new BossMenu();
+		} else {
+			System.out.println("\n잘못된 코드를 입력하셨습니다.");
+			System.out.println("메인 화면으로 돌아갑니다.\n");
+			return;
+		}
+
+		service.start();
 	}
 }
