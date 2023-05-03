326d43843fae7c94a1d37881e3e7e51ff513684a
ny <ny567@naver.com>
2023-05-03 17:57:26 +0900

    230503 초기메뉴로 가기수정

--- pizza/src/com/pizza/custommer/controller/MemberMenu.java
+++ pizza/src/com/pizza/custommer/controller/MemberMenu.java
@@ -17,36 +17,36 @@ import com.pizza.custommer.domain.Member;
 
 public class MemberMenu implements AppService {
 
-	
+
 	private DataBaseConnection connection = 
 			DataBaseConnection.getInstance();
-	
+
 	private Member loginMember = null;
 
 
 	@Override
 	public void start() {
-		memberMenu();
 
 		while(true) {
+			memberMenu();
 			int sel = inputInteger();
 
 			switch (sel) {
 			case 1:
 				login();
-				return;
+				break;
 
 			case 2:
 				changeUser();
-				return;
+				break;
 
 			case 3:
 				deleteUser();
 				return;
 
 			case 4:
-//				order();
-				return;
+				//				order();
+				break;
 
 			default:
 				System.out.print("정확하게 다시 입력해주세요.\n>>> ");
@@ -56,26 +56,23 @@ public class MemberMenu implements AppService {
 
 	}
 
+
+		// 주문
+		private void order() {
+			viewWholeMenu();
+			
 	
-//	// 주문
-//	private void order() {
-//		if(showSearchResult() > 0) {
-//			System.out.print("회원님의 번호를 입력해주세요.\n>>> ");
-//			int loginUserNum = inputInteger();
-//			System.out.print("\n>>> ");
-//
-//
-//			Order order = new Order();
-//			order.setMemberNo(loginUserNum);
-//
-//
-//
-//			orderRepository.addOrder(order);
-//
-//		}
+//				Order order = new Order();
+//				order.setMemberNo(loginUserNum);
+//	
+//	
+//	
+//				orderRepository.addOrder(order);
+//	
+//			}
 
 
-	
+
 
 	//이름, 전화번호 뒷자리 4개로 로그인
 	private void login() {
@@ -102,72 +99,70 @@ public class MemberMenu implements AppService {
 			}
 		}
 		System.out.println("현재 로그인 중인 회원 정보: " + loginMember);
-		
-		while(true) {
-			start();}
-			}
-	
+
+	}
+
 
 
 	//회원의 이름으로 정보 검색
-			public List<Member> findByUserName(String userName) {
-				List<Member> userList = new ArrayList<>();
-				String sql = "SELECT * FROM pizza_members WHERE member_name=?";
-				
-				try(Connection conn = connection.getConnection();
-						PreparedStatement pstmt = conn.prepareStatement(sql)) {
-					pstmt.setString(1, userName);
-					ResultSet rs = pstmt.executeQuery();
-					
-					while(rs.next()) {
-						Member user = new Member(
-									rs.getInt("member_no"),
-									rs.getString("member_name"),
-									rs.getString("b_day"),
-									rs.getString("phone_no"),
-									rs.getString("address")
-									
-								);
-						userList.add(user);
-					}
-					
-				} catch (Exception e) {
-					e.printStackTrace();
-				}	
-				return userList;
+	public List<Member> findByUserName(String userName) {
+		List<Member> userList = new ArrayList<>();
+		String sql = "SELECT * FROM pizza_members WHERE member_name=?";
+
+		try(Connection conn = connection.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(sql)) {
+			pstmt.setString(1, userName);
+			ResultSet rs = pstmt.executeQuery();
+
+			while(rs.next()) {
+				Member user = new Member(
+						rs.getInt("member_no"),
+						rs.getString("member_name"),
+						rs.getString("b_day"),
+						rs.getString("phone_no"),
+						rs.getString("address")
+
+						);
+				userList.add(user);
 			}
-			
-			
-			
-			
-			//회원번호로 정보 수정하기
-			public void changeUser() {
-				if(loginMember != null) {
-				System.out.println("\n수정하실 정보를 선택해주세요.\n1.전화번호 2.집 주소");
-				int cInfoNum=inputInteger();
-				if(cInfoNum == 1) {
-					System.out.println("새로운 전화번호를 입력해주세요.");
-					String nPN=inputString();
-					
-					while((!((nPN.length()==13) && (nPN.charAt(3) == '-') && (nPN.charAt(8)== '-')))) {
-						System.out.println("\n'-'를 포함한 13자리의 형태로 입력해주세요.");
-						System.out.print("전화번호 (XXX-XXXX-XXXX) : "); 
-						nPN = inputString();
-					}
-					
+
+		} catch (Exception e) {
+			e.printStackTrace();
+		}	
+		return userList;
+	}
+
+
+
+
+	//회원번호로 정보 수정하기
+	public void changeUser() {
+		if(loginMember != null) {
+			System.out.println("\n수정하실 정보를 선택해주세요.\n1.전화번호 2.집 주소");
+			int cInfoNum=inputInteger();
+			if(cInfoNum == 1) {
+				System.out.println("새로운 전화번호를 입력해주세요.");
+				String nPN=inputString();
+
+				while((!((nPN.length()==13) && (nPN.charAt(3) == '-') && (nPN.charAt(8)== '-')))) {
+					System.out.println("\n'-'를 포함한 13자리의 형태로 입력해주세요.");
+					System.out.print("전화번호 (XXX-XXXX-XXXX) : "); 
+					nPN = inputString();
+				}
+
 				String sql = "UPDATE pizza_members SET phone_no=? Where member_name=?";
 				try (Connection conn = connection.getConnection();
-					PreparedStatement pstmt = conn.prepareStatement(sql)) {
-						pstmt.setString(1, nPN);
-						pstmt.setString(2, loginMember.getUserName());
-						
-						if(pstmt.executeUpdate() == 1) {
-							System.out.println("\n전화번호가 정상적으로 수정되었습니다.");
-												 
-						}
-	;				}
-						catch (SQLException e) {
-							e.printStackTrace();
+						PreparedStatement pstmt = conn.prepareStatement(sql)) {
+					pstmt.setString(1, nPN);
+					pstmt.setString(2, loginMember.getUserName());
+
+					if(pstmt.executeUpdate() == 1) {
+						System.out.println("\n전화번호가 정상적으로 수정되었습니다.");
+
+					}
+					;				}
+				catch (SQLException e) {
+					e.printStackTrace();
 				}
 			} else if(cInfoNum == 2) {
 				System.out.println("새로운 집 주소를 입력해주세요.");
@@ -175,69 +170,66 @@ public class MemberMenu implements AppService {
 				String sql = "UPDATE pizza_members SET address=? Where member_name=?";
 				try (Connection conn = connection.getConnection();
 						PreparedStatement pstmt = conn.prepareStatement(sql)) {
-							pstmt.setString(1, nAddress);
-							pstmt.setString(2, loginMember.getUserName());
-							
-							if(pstmt.executeUpdate() == 1) {
-								System.out.println("\n집 주소가 정상적으로 수정되었습니다.");
-							} 
-							
+					pstmt.setString(1, nAddress);
+					pstmt.setString(2, loginMember.getUserName());
+
+					if(pstmt.executeUpdate() == 1) {
+						System.out.println("\n집 주소가 정상적으로 수정되었습니다.");
+					} 
+
 				}
-							catch (SQLException e) {
-								e.printStackTrace();
-					}
+				catch (SQLException e) {
+					e.printStackTrace();
+				}
 			}
-			} else {
-				System.out.println("로그인부터 진행해주세요.");
-			}
-				while(true) {
-					start();}
-			}
-			
-			
-			//회원탈퇴
-			public void deleteUser() {
-				if(loginMember != null) {
-					System.out.println("정말로 탈퇴하시겠습니까? (Y/N)");
-					String answer = inputString().toUpperCase();
-					switch (answer.toUpperCase()) {
-						case "Y": case "ㅛ":
-							
-							String sql = "DELETE FROM pizza_members WHERE member_name=?";
-							try(Connection conn = connection.getConnection();
-									PreparedStatement pstmt = conn.prepareStatement(sql)) {
-											pstmt.setString(1, loginMember.getUserName());
-									
-									if(pstmt.executeUpdate() == 1) {
-										System.out.println("\n회원정보가 정상적으로 삭제되었습니다.");
-										System.out.println("처음으로 돌아갑니다.\n");
-						} 
-						} catch (SQLException e) {
-							e.printStackTrace();
-						}
-							break;
-						
-						case "N": case "ㅜ":
-							System.out.println("\n회원 메뉴로 돌아갑니다.\n");
-							break;
-							
-						default:
-							System.out.print("\n정확하게 다시 입력해주세요.\n>>> ");
-							answer = inputString().toUpperCase();
-					
-				
+		} else {
+			System.out.println("로그인부터 진행해주세요.");
+		}
+	}
+
+
+	//회원탈퇴
+	public void deleteUser() {
+		if(loginMember != null) {
+			System.out.println("정말로 탈퇴하시겠습니까? (Y/N)");
+			String answer = inputString().toUpperCase();
+			switch (answer.toUpperCase()) {
+			case "Y": case "ㅛ":
+
+				String sql = "DELETE FROM pizza_members WHERE member_name=?";
+				try(Connection conn = connection.getConnection();
+						PreparedStatement pstmt = conn.prepareStatement(sql)) {
+					pstmt.setString(1, loginMember.getUserName());
+
+					if(pstmt.executeUpdate() == 1) {
+						System.out.println("\n회원정보가 정상적으로 삭제되었습니다.");
+						//비회원 화면으로 가고싶다
+						return;
+					} 
+				} catch (SQLException e) {
+					e.printStackTrace();
+				}
+				break;
+
+			case "N": case "ㅜ":
+				System.out.println("\n회원 메뉴로 돌아갑니다.\n");
+				break;
+
+			default:
+				System.out.print("\n정확하게 다시 입력해주세요.\n>>> ");
+				answer = inputString().toUpperCase();
+
+
 			}} else {
 				System.out.println("로그인부터 진행해주세요.");
 			}
-			
-			while(true) {
-				start();}
-				}
-			}
+		
+	}
+}
 
-	
 
-	
+
+
 
 //	// 검색 결과 출력
 //	private int showSearchResult() {
